8f4252dfeadeb065004dd134a7b95031e1f56485
uttam2311 <2311uttam@mail.com>
2014-12-22 15:58:27 +0530

    modified JVal.py

--- box/JVal.py
+++ box/JVal.py
@@ -2,53 +2,25 @@ import json
 import collections
 
 
-def convert(data):
-    '''
-    converts json object to dictionary
-
-    Input:
-
-    - data: json object
-
-    Return: dictionary
-    '''
-    
-    if isinstance(data, basestring):
-        return str(data)
-    elif isinstance(data, collections.Mapping):
-        return dict(map(convert, data.iteritems()))
-    elif isinstance(data, collections.Iterable):
-        return type(data)(map(convert, data))
-    else:
-        return data
-
 check_list=[]
 def validator(file_name):
 	''' validates and return whether the given json file is valid one are not
 
 	input:json file path
 
-	returns json file if valid, else error message .'''
+	returns True or False .'''
 	data=parse(file_name)
 	if type(data.get("endpoints"))==list:
-		print len(data.get("endpoints"))
-		#print data.get("endpoints")
    		for end_p in data.get("endpoints"):
-			print end_p
 			if type(end_p.get("path"))==unicode and type(end_p.get("method"))==list:	
 				for methods in end_p.get("method"):
-					print "+++++++++++++++++" ,methods
-					print "#######################"
 					for method in methods.keys():
-						print method
-						print ".................."
 						check_list.append(val_met(method,methods.get(method)))
 	for check in check_list:
 		if check==False:
-			print check
-			return "not valid format"
+			return False
 	else:
-		return  data					
+		return  True					
 	
 
 def val_met(method,results):
@@ -59,33 +31,14 @@ def val_met(method,results):
 	'''
 	list_results=[]
 	for result in results.keys():
-		print results.keys()
-		if method=="PUT" or "GET" or "DELETE" and result=="success" or "failure":
+		if (method=="PUT" or "GET" or "DELETE") and ("failure" in results.keys()) and ("success" in results.keys()):
 				if type(results.get(result))==unicode or list:
-					print results.get(result),"**************************"
 					check_list.append(True)	
-		elif method=="POST"and result=="success" or "failure" or "data":
+		elif (method=="POST") and ("failure" in results.keys()) and ("success" in results.keys()) and ("data" in results.keys()):
 			if type(result.get(result))==unicode :
-					print results.get(result),"**************************"
 					check_list.append(True)	
 		else:
 			check_list.append(False)	
-
-'''
-def value_type(key):
-	if key=="name":
-		return str
-        if key=="api":
-		return dict
-	if key=="endpionts":
-		return list
-	if key=="path":
-		return str
-	if key=="method":
-		return list
-	else :
-		return "invalid"
-'''		
 
 def parse(text):
 	'''validates given file is json or not
